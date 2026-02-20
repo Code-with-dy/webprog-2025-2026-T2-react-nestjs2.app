8af4d47d6b4a6326b5be6175afb8a129270fbe35
Code-with-dy <ddvillanueva@student.apc.edu.ph>
2026-02-20 13:05:16 +0000

    updatyed

--- my-profile/backend/src/main.ts
+++ my-profile/backend/src/main.ts
@@ -1,9 +1,14 @@
+import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000, '0.0.0.0');
-  console.log('Backend running on port 3000');
+
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`Backend running on port ${port}`);
 }
 bootstrap();
